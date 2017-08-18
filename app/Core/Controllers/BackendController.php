d0c2c62443ae50b9fc945351ece7d98504dd8d9b
Andrey Borgoyakov <andrey@babenkocommerce.com>
2017-08-18 15:24:50 +0300

    Change public license url

--- app/Core/Controllers/BackendController.php
+++ app/Core/Controllers/BackendController.php
@@ -4,7 +4,7 @@
  * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
  * {Disclaimer}.
  * {Licence}. You can read public licence here
- * @link https://github.com/andrey-borgoyakov/microshop/blob/master/LICENSE
+ * @link https://github.com/andrey-borgoyakov/seo-framework/blob/master/LICENSE
  * {version}.
  * {copyright} Seo Framework. (c) 2017
  * Created by Andrey Borgoyakov. @link
